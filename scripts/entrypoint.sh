f6ccc83545bca2f6aacd0d1b51deead5332216b6
Mohsen Mottaghi <mohsenmottaghi@outlook.com>
2019-12-19 21:41:14 +0330

    change ' to "

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -4,13 +4,13 @@ echo -e "=======================================================================
 echo -e "========================= CLOAK DOMAIN FRONTING =============================="
 echo -e "=============================================================================="
 
-sed -i 's|LOCAL_IP|${LOCAL_IP}|' /app/config.json
-sed -i 's|LOCAL_PORT|${LOCAL_PORT}|' /app/config.json
-sed -i 's|METHOD|${METHOD}|' /app/config.json
-sed -i 's|BYPASSUID|${BYPASSUID}|' /app/config.json
-sed -i 's|REDIRADDR|${REDIRADDR}|' /app/config.json
-sed -i 's|PRIVATEKEY|${PRIVATEKEY}|' /app/config.json
-sed -i 's|ADMINUID|${ADMINUID}|' /app/config.json
+sed -i "s|LOCAL_IP|${LOCAL_IP}|" /app/config.json
+sed -i "s|LOCAL_PORT|${LOCAL_PORT}|" /app/config.json
+sed -i "s|METHOD|${METHOD}|" /app/config.json
+sed -i "s|BYPASSUID|${BYPASSUID}|" /app/config.json
+sed -i "s|REDIRADDR|${REDIRADDR}|" /app/config.json
+sed -i "s|PRIVATEKEY|${PRIVATEKEY}|" /app/config.json
+sed -i "s|ADMINUID|${ADMINUID}|" /app/config.json
 
 echo -e '[+] Config.json generated successfully.'
 echo -e '[+] Show Container config'
