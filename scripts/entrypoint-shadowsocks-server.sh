e3f06990970369b20491b131875a52b216d42a5b
Mohsen Mottaghi <mohsenmottaghi@outlook.com>
2019-12-20 10:59:18 +0330

    add shadowsocks dockerfile

---
+++ scripts/entrypoint-shadowsocks-server.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+echo -e "=============================================================================="
+echo -e "========================= SHADOWSOCKS SERVER GO =============================="
+echo -e "=============================================================================="
+
+echo -e '[+] Show Container config'
+echo -e "[!] Server IP : ${SERVER_IP}"
+echo -e "[!] Server Port : ${SERVER_PORT}"
+echo -e "[!] Encryption Method: ${ENCRYPTION}"
+echo -e "[!] Password : ${PASSWORD}"
+
+exec shadowsocks-server -s "ss://${ENCRYPTION}:${PASSWORD}@${SERVER_IP}:${SERVER_PORT}" -verbose
+
+exec "$@"
