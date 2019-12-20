206c43b31938983ffaaca6768b893bec2286e1aa
Mohsen Mottaghi <mohsenmottaghi@outlook.com>
2019-12-20 11:14:38 +0330

    add shadowsocks client Dockerfile

--- scripts/entrypoint-shadowsocks-client.sh
+++ scripts/entrypoint-shadowsocks-client.sh
@@ -7,9 +7,11 @@ echo -e "=======================================================================
 echo -e '[+] Show Container config'
 echo -e "[!] Server IP : ${SERVER_IP}"
 echo -e "[!] Server Port : ${SERVER_PORT}"
+echo -e "[!] Local IP : ${LOCAL_IP}"
+echo -e "[!] Local Port : ${LOCAL_PORT}"
 echo -e "[!] Encryption Method: ${ENCRYPTION}"
 echo -e "[!] Password : ${PASSWORD}"
 
-exec /app/shadowsocks-server -s "ss://${ENCRYPTION}:${PASSWORD}@${SERVER_IP}:${SERVER_PORT}"
+exec /app/shadowsocks-client -c "ss://${ENCRYPTION}:${PASSWORD}@${SERVER_IP}:${SERVER_PORT}" -socks ${LOCAL_IP}:${LOCAL_PORT} -u
 
 exec "$@"
