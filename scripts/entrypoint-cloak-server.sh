e9d918699a34555a481083d16fc096eda4d86c03
TheOldGoldenFish <45272005+TheOldGoldenFish@users.noreply.github.com>
2022-11-20 00:08:16 +0330

    Update entrypoint-cloak-server.sh

--- scripts/entrypoint-cloak-server.sh
+++ scripts/entrypoint-cloak-server.sh
@@ -6,11 +6,11 @@ echo -e "=======================================================================
 
 sed -i "s|LOCAL_IP|${LOCAL_IP}|" /app/config.json
 sed -i "s|LOCAL_PORT|${LOCAL_PORT}|" /app/config.json
-sed -i "s|METHOD|${METHOD}|" /app/config.json
 sed -i "s|BYPASSUID|${BYPASSUID}|" /app/config.json
-sed -i "s|REDIRADDR|${REDIRADDR}|" /app/config.json
+sed -i "s|SERVERNAME|${REDIRADDR}|" /app/config.json
 sed -i "s|PRIVATEKEY|${PRIVATEKEY}|" /app/config.json
 sed -i "s|ADMINUID|${ADMINUID}|" /app/config.json
+sed -i "s|SERVERNAME|${REDIRADDR}|" /app/config.json
 
 
 echo -e '[+] Config.json generated successfully.'
@@ -19,7 +19,7 @@ echo -e "[!] Local IP : \t\t${LOCAL_IP}"
 echo -e "[!] Local Port : \t${LOCAL_PORT}"
 echo -e "[!] Method : \t\t${METHOD}"
 echo -e "[!] BypassUID : \t${BYPASSUID}"
-echo -e "[!] RedirAddr : \t${REDIRADDR}"
+echo -e "[!] Server Name : \t${REDIRADDR}"
 echo -e "[!] PrivateKey : \t${PRIVATEKEY}"
 echo -e "[!] AdminUID : \t\t${ADMINUID}"
 echo -e "[!] PublickKey : \t${PUBLICKKEY}"
