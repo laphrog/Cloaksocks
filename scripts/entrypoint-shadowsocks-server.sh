2f99b8db42ba8026c6d769ab3e758fe3799c59bf
TheOldGoldenFish <45272005+TheOldGoldenFish@users.noreply.github.com>
2022-11-21 02:25:42 +0330

    Update entrypoint-shadowsocks-server.sh

--- scripts/entrypoint-shadowsocks-server.sh
+++ scripts/entrypoint-shadowsocks-server.sh
@@ -9,7 +9,6 @@ echo -e "[!] Server IP : \t${SERVER_IP}"
 echo -e "[!] Server Port : \t${SERVER_PORT}"
 echo -e "[!] Encryption Method:  ${ENCRYPTION}"
 echo -e "[!] Password : \t\t${PASSWORD}"
-echo -e "[+] See Cloak container logs for Connection info."
 
 exec /app/shadowsocks-server -s "ss://${ENCRYPTION}:${PASSWORD}@${SERVER_IP}:${SERVER_PORT}"
 
